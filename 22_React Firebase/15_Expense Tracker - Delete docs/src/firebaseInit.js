40fd5c9f705837c84203a7b6d4d9aac30c8525ac
raj <rpande400@gmail.com>
2023-07-30 23:23:06 +0530

    React firebase

---
+++ 22_React Firebase/15_Expense Tracker - Delete docs/src/firebaseInit.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCr6DPg4U-7fAu8Yzo2Xxd0_JVS-T8x800",
+  authDomain: "expense-tracker--app.firebaseapp.com",
+  projectId: "expense-tracker--app",
+  storageBucket: "expense-tracker--app.appspot.com",
+  messagingSenderId: "1088078138498",
+  appId: "1:1088078138498:web:dd95cebd10e0556f0fb89b",
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
